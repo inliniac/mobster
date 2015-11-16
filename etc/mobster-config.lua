38d8e2065b706a1338234bb60b735f02cc23f59e
Randy Caldejon <randy@packetchaser.org>
2015-11-16 02:35:40 +0000

    Changed changed the paths in mobster-config.lua to something more generic.

--- etc/mobster-config.lua
+++ etc/mobster-config.lua
@@ -4,15 +4,17 @@
 
 redis_host="127.0.0.1"
 redis_port=6379
+
+-- changed the following to ${MOBSTER}/lua directory
 event_threads =
 {
-	"/home/randy/mobster/lua/file-events.lua",
-	"/home/randy/mobster/lua/dns-events.lua",
-	"/home/randy/mobster/lua/ssh-events.lua",
-	"/home/randy/mobster/lua/tls-events.lua",
-	"/home/randy/mobster/lua/http-events.lua",
-	"/home/randy/mobster/lua/flow-events.lua",
-	"/home/randy/mobster/lua/sumstats-events.lua"
+	"/home/github/mobster/lua/file-events.lua",
+	"/home/github/mobster/lua/dns-events.lua",
+	"/home/github/mobster/lua/ssh-events.lua",
+	"/home/github/mobster/lua/tls-events.lua",
+	"/home/github/mobster/lua//http-events.lua",
+	"/home/github/mobster/lua/flow-events.lua",
+	"/home/github/mobster/lua/sumstats-events.lua"
 }
 
 
