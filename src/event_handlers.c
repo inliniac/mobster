c2fce2e8a7ff936c396b4dc67e97e365e5aaaed3
Victor Julien <victor@inliniac.net>
2016-01-04 18:38:51 +0100

    style: 4 space indent, add vim headers

--- src/event_handlers.c
+++ src/event_handlers.c
@@ -21,6 +21,8 @@
  *
  */
 
+/* vim: noai:ts=4:sw=4 */
+
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -39,9 +41,9 @@
 
 #include <hiredis/hiredis.h>
 
-#include <lua.h>                               
-#include <lauxlib.h>                            
-#include <lualib.h>                             
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
 
 #ifdef ENABLE_JIT
 #include <luajit-2.0/luajit.h>
@@ -54,7 +56,7 @@ static char *log_dir = "/var/log/mobster";
 static char *log_file = "mobster.log";
 static int g_redis_port = 6379;
 
-static int g_verbose = 0; 
+static int g_verbose = 0;
 static redisContext *g_redis_ctx = NULL;
 static pthread_mutex_t g_redis_mutex = PTHREAD_MUTEX_INITIALIZER;
 static FILE *g_fp = NULL;
@@ -69,27 +71,27 @@ extern uint64_t g_running;
  */
 void file_rotate(int signo)
 {
-  	if (signo == SIGUSR1)
-	{
-		char newfile [PATH_MAX];
+    if (signo == SIGUSR1)
+    {
+        char newfile [PATH_MAX];
 
-		pthread_mutex_lock(&g_redis_mutex);
-		fclose (g_fp);
+        pthread_mutex_lock(&g_redis_mutex);
+        fclose (g_fp);
 
-                char filename [PATH_MAX];
-                snprintf(filename, PATH_MAX, "%s/%s", log_dir, log_file);
-		g_fp = fopen (filename, "w+");
-		if (!g_fp)
-		{
-			perror ("fopen");
-			pthread_mutex_unlock(&g_redis_mutex);
-			abort ();
-		}
-		setvbuf(g_fp, NULL, _IOLBF, 0);
+        char filename [PATH_MAX];
+        snprintf(filename, PATH_MAX, "%s/%s", log_dir, log_file);
+        g_fp = fopen (filename, "w+");
+        if (!g_fp)
+        {
+            perror ("fopen");
+            pthread_mutex_unlock(&g_redis_mutex);
+            abort ();
+        }
+        setvbuf(g_fp, NULL, _IOLBF, 0);
 
-		pthread_mutex_unlock(&g_redis_mutex);
-		syslog (LOG_ERR,"%s: received HUP to truncate %s/%s", log_dir, log_file);
-	}
+        pthread_mutex_unlock(&g_redis_mutex);
+        syslog (LOG_ERR,"%s: received HUP to truncate %s/%s", log_dir, log_file);
+    }
 }
 
 /*
@@ -99,64 +101,64 @@ void file_rotate(int signo)
  */
 static void *notice_thread (void *v)
 {
-	(void) v;
+    (void) v;
 
-        if (signal(SIGUSR1, file_rotate) == SIG_ERR)
-	{
-		syslog (LOG_ERR,"signal() failed; %s", strerror (errno));
-		abort ();
-	}
+    if (signal(SIGUSR1, file_rotate) == SIG_ERR)
+    {
+        syslog (LOG_ERR,"signal() failed; %s", strerror (errno));
+        abort ();
+    }
 
-	if (mkdir(log_dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0)
-	{
-		if (errno != EEXIST)
-		{
-			syslog (LOG_ERR,"mkdir %s; %s", log_dir, strerror (errno));
-			abort ();
-		}
-	}
-
-        char filename [PATH_MAX];
-        snprintf(filename, PATH_MAX, "%s/%s", log_dir, log_file);
-	g_fp = fopen (filename, "a+");
-	if (!g_fp)
-	{
-		perror ("fopen");
-		abort ();
-	}
-	setvbuf(g_fp, NULL, _IOLBF, 0);
-
-    	redisContext *redis_ctx = redisConnect(g_redis_host, g_redis_port);
-    	if (redis_ctx != NULL && redis_ctx->err)
-    	{
-        	syslog (LOG_ERR,"redisConnect() failed; %s", redis_ctx->errstr);
-        	abort ();
-    	}
-
-	redisReply *reply = redisCommand(redis_ctx,"SUBSCRIBE %s", notice_key);
-
-	if (reply->type==REDIS_REPLY_ERROR)
+    if (mkdir(log_dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0)
+    {
+        if (errno != EEXIST)
         {
-        	syslog (LOG_ERR,"redisCommand() failed; %s", reply->str);
-		abort ();
+            syslog (LOG_ERR,"mkdir %s; %s", log_dir, strerror (errno));
+            abort ();
         }
-	if (reply) freeReplyObject(reply);
-	
-       	syslog (LOG_INFO,"Notice thread running");
-	while(redisGetReply(redis_ctx,(void **) &reply) == REDIS_OK)
-	{
-		if (reply->type==REDIS_REPLY_ARRAY && reply->elements>2)
-		{
-			pthread_mutex_lock(&g_redis_mutex);
-			fputs (reply->element[2]->str, g_fp);
-			fputs ("\n", g_fp);
-			pthread_mutex_unlock(&g_redis_mutex);
-			if (g_verbose) puts (reply->element[2]->str);
-		}
-    		if (reply) freeReplyObject(reply);
-	}
-	fclose (g_fp);
-	redisFree (redis_ctx);
+    }
+
+    char filename [PATH_MAX];
+    snprintf(filename, PATH_MAX, "%s/%s", log_dir, log_file);
+    g_fp = fopen (filename, "a+");
+    if (!g_fp)
+    {
+        perror ("fopen");
+        abort ();
+    }
+    setvbuf(g_fp, NULL, _IOLBF, 0);
+
+    redisContext *redis_ctx = redisConnect(g_redis_host, g_redis_port);
+    if (redis_ctx != NULL && redis_ctx->err)
+    {
+        syslog (LOG_ERR,"redisConnect() failed; %s", redis_ctx->errstr);
+        abort ();
+    }
+
+    redisReply *reply = redisCommand(redis_ctx,"SUBSCRIBE %s", notice_key);
+
+    if (reply->type==REDIS_REPLY_ERROR)
+    {
+        syslog (LOG_ERR,"redisCommand() failed; %s", reply->str);
+        abort ();
+    }
+    if (reply) freeReplyObject(reply);
+
+    syslog (LOG_INFO,"Notice thread running");
+    while(redisGetReply(redis_ctx,(void **) &reply) == REDIS_OK)
+    {
+        if (reply->type==REDIS_REPLY_ARRAY && reply->elements>2)
+        {
+            pthread_mutex_lock(&g_redis_mutex);
+            fputs (reply->element[2]->str, g_fp);
+            fputs ("\n", g_fp);
+            pthread_mutex_unlock(&g_redis_mutex);
+            if (g_verbose) puts (reply->element[2]->str);
+        }
+        if (reply) freeReplyObject(reply);
+    }
+    fclose (g_fp);
+    redisFree (redis_ctx);
     pthread_exit(NULL);
 }
 
@@ -165,32 +167,32 @@ static void *notice_thread (void *v)
  *
  * ---------------------------------------------------------------------------------------
  */
-static int mobster_notify(lua_State *L) 
+static int mobster_notify(lua_State *L)
 {
-	const char *timestamp = luaL_checkstring(L, 1);
-        const char *category = luaL_checkstring(L, 2);
-        const char *action = luaL_checkstring(L, 3);
-        const char *message = luaL_checkstring(L, 4);
+    const char *timestamp = luaL_checkstring(L, 1);
+    const char *category = luaL_checkstring(L, 2);
+    const char *action = luaL_checkstring(L, 3);
+    const char *message = luaL_checkstring(L, 4);
 
-	 char json [1024];
-	 memset (json, 0, sizeof(json));
-	 snprintf (json, 
-	    	sizeof(json)-1, 
-		"{\"timestamp\":\"%s\",\"event_type\":\"notice\",\"notice\":{\"category\":\"%s\",\"action\":\"%s\",\"message\":\"%s\"}}",
-	  	timestamp, category, action, message);
-	 if (g_verbose) fprintf (stderr,"%s: %s\n", __FUNCTION__, json);
-         
-	 redisReply *reply = redisCommand(g_redis_ctx,"PUBLISH %s %s", notice_key, json);
+    char json [1024];
+    memset (json, 0, sizeof(json));
+    snprintf (json,
+            sizeof(json)-1,
+            "{\"timestamp\":\"%s\",\"event_type\":\"notice\",\"notice\":{\"category\":\"%s\",\"action\":\"%s\",\"message\":\"%s\"}}",
+            timestamp, category, action, message);
+    if (g_verbose) fprintf (stderr,"%s: %s\n", __FUNCTION__, json);
 
-	 if (reply)
-	 {
-	 	freeReplyObject(reply);
-	 }
-	 else
-	 {
-       		syslog (LOG_ERR,"PUBLISH - %s", g_redis_ctx->errstr);
-	 }
-	 return 0;
+    redisReply *reply = redisCommand(g_redis_ctx,"PUBLISH %s %s", notice_key, json);
+
+    if (reply)
+    {
+        freeReplyObject(reply);
+    }
+    else
+    {
+        syslog (LOG_ERR,"PUBLISH - %s", g_redis_ctx->errstr);
+    }
+    return 0;
 }
 
 /*
@@ -214,12 +216,12 @@ static void *lua_thread (void *ptr)
     if (slash) name = (slash+1);
     if (name)
     {
-	pthread_setname_np(pthread_self(), name);
+        pthread_setname_np(pthread_self(), name);
     }
 
-    lua_State *L = luaL_newstate();                       
-    luaL_openlibs(L);  
-  
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
     syslog (LOG_INFO, "Loading %s", lua_script);
     lua_pushstring(L, script_dir);
     lua_setglobal(L, "script_dir");
@@ -229,10 +231,10 @@ static void *lua_thread (void *ptr)
     if (luaL_loadfile(L, lua_script)||lua_pcall(L, 0, 0, 0))
     {
         syslog (LOG_ERR, "luaL_loadfile %s failed - %s",lua_script, lua_tostring(L, -1));
-    	pthread_exit(NULL);
+        pthread_exit(NULL);
     }
 
-    /* globally set redis host and port number in the LUA engine */ 
+    /* globally set redis host and port number in the LUA engine */
     lua_pushnumber(L, g_redis_port);
     lua_setglobal(L, "redis_port");
     lua_pushstring(L, g_redis_host);
@@ -244,13 +246,13 @@ static void *lua_thread (void *ptr)
 
 
     syslog (LOG_NOTICE,"Running %s as %s\n", lua_script, name);
-    lua_getglobal(L, "run");             
-    if (lua_pcall(L, 0, 0, 0))  
+    lua_getglobal(L, "run");
+    if (lua_pcall(L, 0, 0, 0))
     {
         syslog (LOG_ERR,"lua_pcall run(%s) failed; %s",lua_script, lua_tostring(L, -1));
-    	pthread_exit(NULL);
-    }          
-    lua_close(L);         
+        pthread_exit(NULL);
+    }
+    lua_close(L);
     syslog (LOG_NOTICE,"%s exiting", (name?name:"thread"));
     pthread_exit(NULL);
 }
@@ -265,41 +267,41 @@ static void run_mobster (const char* mobster_config)
 {
     struct stat sb;
 
-    lua_State *L = luaL_newstate();                       
-    luaL_openlibs(L);                           
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
 
     if (luaL_loadfile(L, mobster_config)) 
     {
         syslog (LOG_ERR,"luaL_loadfile failed; %s",lua_tostring(L, -1));
-	abort ();
+        abort ();
     }
 
-    if (lua_pcall(L, 0, 0, 0))                  
+    if (lua_pcall(L, 0, 0, 0))
     {
         syslog (LOG_ERR,"lua_pcall failed; %s",lua_tostring(L, -1));
-	abort ();
+        abort ();
     }
 
     lua_getglobal(L, "redis_host");
     if (lua_isstring(L, -1))
     {
-	g_redis_host = strdup(lua_tostring(L, -1));
+        g_redis_host = strdup(lua_tostring(L, -1));
     }
 
     lua_getglobal(L, "redis_port");
     if (lua_isnumber(L, -1))
     {
-	g_redis_port = (int) lua_tonumber(L, -1);
+        g_redis_port = (int) lua_tonumber(L, -1);
     }
 
     /* Added the following options below to allow customization of running instances */
-    lua_getglobal(L, "notice_key");             
+    lua_getglobal(L, "notice_key");
     if (lua_isstring(L, -1))
     {
         notice_key = strdup(lua_tostring(L, -1));
     }
 
-    lua_getglobal(L, "script_dir");             
+    lua_getglobal(L, "script_dir");
 
     if (lua_isstring(L, -1))
     {
@@ -308,21 +310,21 @@ static void run_mobster (const char* mobster_config)
 
     if ((lstat (script_dir,&sb)<0) || !S_ISDIR(sb.st_mode))
     {
-       char* mobster_root=getenv(MOBSTER_ROOT);
-       char temp_script_dir [PATH_MAX];
+        char* mobster_root=getenv(MOBSTER_ROOT);
+        char temp_script_dir [PATH_MAX];
 
-       snprintf (temp_script_dir, PATH_MAX, "%s/%s", mobster_root, "/scripts");
+        snprintf (temp_script_dir, PATH_MAX, "%s/%s", mobster_root, "/scripts");
 
-       if (!temp_script_dir || !mobster_root)
-       {
-          fprintf (stderr,"Script Directory %s does not exist and cannot location under mobster_root\n", script_dir);
-          exit (EXIT_FAILURE);
-       }
-       else
-       {  
-          fprintf(stderr,"Script Directory %s does not exist.  Using Script directory under mobster_root\n", script_dir);
-          script_dir=temp_script_dir;
-       }
+        if (!temp_script_dir || !mobster_root)
+        {
+            fprintf (stderr,"Script Directory %s does not exist and cannot location under mobster_root\n", script_dir);
+            exit (EXIT_FAILURE);
+        }
+        else
+        {
+            fprintf(stderr,"Script Directory %s does not exist.  Using Script directory under mobster_root\n", script_dir);
+            script_dir=temp_script_dir;
+        }
     }
 
     lua_getglobal(L, "log_dir");
@@ -338,56 +340,56 @@ static void run_mobster (const char* mobster_config)
     }
     /* End of new keys added */
 
-    lua_getglobal(L, "mobster_scripts");             
+    lua_getglobal(L, "mobster_scripts");
 
-    lua_pushnil(L);  
+    lua_pushnil(L);
 
     if (!lua_istable(L, -2))
     {
         syslog (LOG_ERR,"Configuration item mobster_scripts must be a table");
-    	pthread_exit(NULL);
+        pthread_exit(NULL);
     }
 
     g_redis_ctx = redisConnect(g_redis_host, g_redis_port);
     if (g_redis_ctx != NULL && g_redis_ctx->err)
     {
-    	syslog (LOG_ERR,"redisConnect() failed; %s", g_redis_ctx->errstr);
-	abort ();
+        syslog (LOG_ERR,"redisConnect() failed; %s", g_redis_ctx->errstr);
+        abort ();
     }
 
     pthread_t thread;
     if(pthread_create(&thread, NULL, notice_thread, (void *) NULL)!=0)
     {
-	syslog (LOG_ERR,"pthread_create() %s", strerror (errno));
-	pthread_exit(NULL);
+        syslog (LOG_ERR,"pthread_create() %s", strerror (errno));
+        pthread_exit(NULL);
     }
 
-    while (lua_next(L, -2)) 
-    {                   
-	struct stat sb;
-        const char *lua_script = lua_tostring(L, -1);                 
-	char tmp [PATH_MAX];
-	snprintf (tmp, PATH_MAX, "%s/%s", script_dir, lua_script);
+    while (lua_next(L, -2))
+    {
+        struct stat sb;
+        const char *lua_script = lua_tostring(L, -1);
+        char tmp [PATH_MAX];
+        snprintf (tmp, PATH_MAX, "%s/%s", script_dir, lua_script);
 
-	/*
-         * check that file exists with execute permissions 
+        /*
+         * check that file exists with execute permissions
          */
-	if ((lstat (tmp,&sb)>=0) && S_ISREG(sb.st_mode))
-	{
-		char *mobster_script = strndup((const char *)tmp, PATH_MAX);
-		if(pthread_create(&thread, NULL, lua_thread, (void *) mobster_script)!=0)
-		{
-			syslog (LOG_ERR,"pthread_create() %s", strerror (errno));
-    			pthread_exit(NULL);
-		}
-	}
-	else
-	{
-		syslog (LOG_ERR,"Mobster script %s does not exist.", lua_script);
-	}
-        lua_pop(L,1);                           
+        if ((lstat (tmp,&sb)>=0) && S_ISREG(sb.st_mode))
+        {
+            char *mobster_script = strndup((const char *)tmp, PATH_MAX);
+            if(pthread_create(&thread, NULL, lua_thread, (void *) mobster_script)!=0)
+            {
+                syslog (LOG_ERR,"pthread_create() %s", strerror (errno));
+                pthread_exit(NULL);
+            }
+        }
+        else
+        {
+            syslog (LOG_ERR,"Mobster script %s does not exist.", lua_script);
+        }
+        lua_pop(L,1);
     }
-    lua_close(L);                               
+    lua_close(L);
 }
 
 /*
--- src/mobster.c
+++ src/mobster.c
@@ -15,11 +15,13 @@
  * 02110-1301, USA.
  */
 
-/* 
+/*
  *
  * author Randy Caldejon <randy@packetchaser.org>
  *
  */
+
+/* vim: noai:ts=4:sw=4 */
 
 #define _GNU_SOURCE
 
@@ -49,93 +51,93 @@ uint64_t g_running = 1;
  *
  * ---------------------------------------------------------------------------------------
  */
- 
+
 int main(int argc, char **argv)
 {
-	struct stat sb;
+    struct stat sb;
 
-        char* mobster_root;
-        char* mobster_config;
-        if ( argc > 2 ) /* Check to see if too many arguments were presented */
+    char* mobster_root;
+    char* mobster_config;
+    if ( argc > 2 ) /* Check to see if too many arguments were presented */
+    {
+        fprintf (stderr, "Too many arguments supplied\n");
+        exit (EXIT_FAILURE);
+    }
+    else if ( argc == 2) /* Second Option is location of config file */
+    {
+        if ((lstat (argv[1],&sb)<0) || !S_ISREG(sb.st_mode))
         {
-           fprintf (stderr, "Too many arguments supplied\n");
-           exit (EXIT_FAILURE);
+            mobster_root=getenv(MOBSTER_ROOT);
+            char config_file [PATH_MAX];
+            snprintf (config_file, PATH_MAX, "%s/%s", mobster_root, "/scripts/config.lua");
+
+            if (!mobster_root)
+            {
+                fprintf (stderr,"Config file %s and environment variable MOBSTER_ROOT do not exist\n", argv[1]);
+                exit (EXIT_FAILURE);
+            }
+            else if ((lstat (config_file,&sb)<0) || !S_ISREG(sb.st_mode))
+            {
+                fprintf (stderr,"Config file specified does not exist. $MOBSTER_ROOT/scripts/config.lua does not exist\n");
+                exit (EXIT_FAILURE);
+            }
+            else
+            {
+                mobster_config=config_file;
+            }
         }
-        else if ( argc == 2) /* Second Option is location of config file */
+        else
         {
-           if ((lstat (argv[1],&sb)<0) || !S_ISREG(sb.st_mode))
-           {
-	      mobster_root=getenv(MOBSTER_ROOT);
-              char config_file [PATH_MAX];
-              snprintf (config_file, PATH_MAX, "%s/%s", mobster_root, "/scripts/config.lua");
-
-              if (!mobster_root)
-              {
-                 fprintf (stderr,"Config file %s and environment variable MOBSTER_ROOT do not exist\n", argv[1]);
-                 exit (EXIT_FAILURE);
-              }
-              else if ((lstat (config_file,&sb)<0) || !S_ISREG(sb.st_mode))
-              {
-                 fprintf (stderr,"Config file specified does not exist. $MOBSTER_ROOT/scripts/config.lua does not exist\n");
-                 exit (EXIT_FAILURE);
-              }
-              else
-              {
-                 mobster_config=config_file;
-              }
-           }
-           else
-           {
-              mobster_config=argv[1];
-           }
+            mobster_config=argv[1];
         }
-        else /* Otherwise look for mobster_root */
-	{
-           mobster_root=getenv(MOBSTER_ROOT);
+    }
+    else /* Otherwise look for mobster_root */
+    {
+        mobster_root=getenv(MOBSTER_ROOT);
 
-	   if (!mobster_root)
-	   {
-	      fprintf (stderr,"Must define environment variable MOBSTER_ROOT\n");
-	      exit (EXIT_FAILURE);
-	   }
- 	   if ((lstat (mobster_root,&sb)<0) || !S_ISDIR(sb.st_mode))
-	   {
-	      fprintf (stderr,"MOBSTER_ROOT %s does not exist\n", mobster_root);
-	      exit (EXIT_FAILURE);
-	   }
-           char config_file [PATH_MAX];
-           snprintf (config_file, PATH_MAX, "%s/%s", mobster_root, "/scripts/config.lua");
-           if ((lstat (config_file,&sb)<0) || !S_ISREG(sb.st_mode))
-           {
-              fprintf (stderr,"$MOBSTER_ROOT/scripts/config.lua does not exist\n.");
-              exit (EXIT_FAILURE);
-           }
-           mobster_config=config_file;
-        }
-        int path_len = strlen(mobster_config);
-        if (path_len > PATH_MAX)
+        if (!mobster_root)
         {
-           fprintf (stderr,"Pathname of config file is too large (%i characters)", path_len);
-           exit (EXIT_FAILURE);
+            fprintf (stderr,"Must define environment variable MOBSTER_ROOT\n");
+            exit (EXIT_FAILURE);
         }
+        if ((lstat (mobster_root,&sb)<0) || !S_ISDIR(sb.st_mode))
+        {
+            fprintf (stderr,"MOBSTER_ROOT %s does not exist\n", mobster_root);
+            exit (EXIT_FAILURE);
+        }
+        char config_file [PATH_MAX];
+        snprintf (config_file, PATH_MAX, "%s/%s", mobster_root, "/scripts/config.lua");
+        if ((lstat (config_file,&sb)<0) || !S_ISREG(sb.st_mode))
+        {
+            fprintf (stderr,"$MOBSTER_ROOT/scripts/config.lua does not exist\n.");
+            exit (EXIT_FAILURE);
+        }
+        mobster_config=config_file;
+    }
+    int path_len = strlen(mobster_config);
+    if (path_len > PATH_MAX)
+    {
+        fprintf (stderr,"Pathname of config file is too large (%i characters)", path_len);
+        exit (EXIT_FAILURE);
+    }
 
-	signal(SIGPIPE, SIG_IGN);
-	openlog ("mobster", LOG_PERROR, LOG_USER);
+    signal(SIGPIPE, SIG_IGN);
+    openlog ("mobster", LOG_PERROR, LOG_USER);
 
-        pthread_setname_np(pthread_self(), "mobster");
+    pthread_setname_np(pthread_self(), "mobster");
 
-	if (mobster_start (mobster_config) < 0)
-	{
-		syslog (LOG_ERR,"mobster_start() failed");
-		return (EXIT_FAILURE);
-	}
-	while (g_running)
-	{
-		sleep(1);
-	}
-	closelog ();
-	
-	return (EXIT_SUCCESS);		
+    if (mobster_start (mobster_config) < 0)
+    {
+        syslog (LOG_ERR,"mobster_start() failed");
+        return (EXIT_FAILURE);
+    }
+    while (g_running)
+    {
+        sleep(1);
+    }
+    closelog ();
+
+    return (EXIT_SUCCESS);
 }
 
 /*
